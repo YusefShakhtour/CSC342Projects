02aedf85f443422272d889b0ccfa83948bf3d54a
yfshakht <yfshakht@ncsu.edu>
2024-02-23 12:18:51 -0500

    [Fix Attempt]

--- Homework3/static/js/form.js
+++ Homework3/static/js/form.js
@@ -7,8 +7,10 @@ document.addEventListener('DOMContentLoaded', e => {
     let expDate = document.getElementById("date");
     let textPhone = document.getElementById("textPhone");
     let textEmail = document.getElementById("textEmail");
-    let msg = document.getElementById('msg');
+    let msg = document.querySelector('#msg');
     let cardNum = document.getElementById('card');
+
+    let form = document.querySelector("form");
 
     let check = document.getElementById('check');
     let file = document.getElementById("fileIn");
@@ -48,32 +50,35 @@ document.addEventListener('DOMContentLoaded', e => {
         }
     });
 
-    cardNum.setCustomValidity("Please enter your card in the following format ####-####-####-####");
-    check.setCustomValidity("You must agree to our terms and conditions to use this service");
+    // cardNum.setCustomValidity("Please enter your card in the following format ####-####-####-####");
+    // check.setCustomValidity("You must agree to our terms and conditions to use this service");
+    // msg.setCustomValidity("Your message should be at least 10 characters, but no more than 500 characters");
+    // let date = expDate.value;
+    // let split = date.split("/");
+    // let exp = new Date(split[1], split[0]);
+    // let today = new Date();
+    // if (!expDate.checkValidity()) {
+    //     expDate.setCustomValidity("Please match the following format: MM/YYYY");
+    // }
+    // else if (exp < today) {
+    //     expDate.setCustomValidity("The card you entered is expired")
+    // }
+
+    // if (!textPhone.checkValidity() && radioSMS.checked) {
+    //     textPhone.setCustomValidity("Please enter the 10 digit phone number you would like to notify ##########");
+    // }
+
+    // if (!textEmail.checkValidity() && radioEmail.checked) {
+    //     textPhone.setCustomValidity("Please enter the email you would like to notify");
+    // }
+
+    // if (!check.checkValidity()) {
+    //     console.log(check.checkValidity());
+    //     check.setCustomValidity("You must agree to our terms and conditions to use this service");
+    // }
+
+
     msg.setCustomValidity("Your message should be at least 10 characters, but no more than 500 characters");
-    let date = expDate.value;
-    let split = date.split("/");
-    let exp = new Date(split[1], split[0]);
-    let today = new Date();
-    if (!expDate.checkValidity()) {
-        expDate.setCustomValidity("Please match the following format: MM/YYYY");
-    }
-    else if (exp < today) {
-        expDate.setCustomValidity("The card you entered is expired")
-    }
-
-    if (!textPhone.checkValidity() && radioSMS.checked) {
-        textPhone.setCustomValidity("Please enter the 10 digit phone number you would like to notify ##########");
-    }
-
-    if (!textEmail.checkValidity() && radioEmail.checked) {
-        textPhone.setCustomValidity("Please enter the email you would like to notify");
-    }
-
-    if (!check.checkValidity()) {
-        console.log(check.checkValidity());
-        check.setCustomValidity("You must agree to our terms and conditions to use this service");
-    }
 
     submit.addEventListener('click', function(e) {
         // console.log(cardNum.checkValidity());
@@ -81,36 +86,80 @@ document.addEventListener('DOMContentLoaded', e => {
         // console.log(expDate.checkValidity());
         // console.log(check.checkValidity());
         // console.log(file.checkValidity());
+        console.log("Submit attempt");
+
+        if (msg.checkValidity() == false) {
+            msg.setCustomValidity("Your message should be at least 10 characters, but no more than 500 characters");
+        }
+        else {
+            msg.setCustomValidity(null);
+        }
 
 
-        // if (msg.checkValidity() == false) {
-        //     msg.setCustomValidity("Your message should be at least 10 characters, but no more than 500 characters");
-        // }
 
-        // let date = expDate.value;
-        // let split = date.split("/");
-        // let exp = new Date(split[1], split[0]);
-        // let today = new Date();
-        // if (!expDate.checkValidity()) {
-        //     expDate.setCustomValidity("Please match the following format: MM/YYYY");
-        // }
-        // else if (exp < today) {
-        //     expDate.setCustomValidity("The card you entered is expired")
-        // }
+        let date = expDate.value;
+        let split = date.split("/");
+        let exp = new Date(split[1], split[0]);
+        let today = new Date();
+        if (!expDate.checkValidity()) {
+            expDate.setCustomValidity("Please match the following format: MM/YYYY");
+        }
+        else if (exp < today) {
+            expDate.setCustomValidity("The card you entered is expired")
+        }
         
 
-        // if (!textPhone.checkValidity() && radioSMS.checked) {
-        //     textPhone.setCustomValidity("Please enter the 10 digit phone number you would like to notify ##########");
-        // }
+        if (!textPhone.checkValidity() && radioSMS.checked) {
+            textPhone.setCustomValidity("Please enter the 10 digit phone number you would like to notify ##########");
+        }
 
-        // if (!textEmail.checkValidity() && radioEmail.checked) {
-        //     textPhone.setCustomValidity("Please enter the email you would like to notify");
-        // }
+        if (!textEmail.checkValidity() && radioEmail.checked) {
+            textPhone.setCustomValidity("Please enter the email you would like to notify");
+        }
 
-        // if (!check.checkValidity()) {
-        //     console.log(check.checkValidity());
-        //     check.setCustomValidity("You must agree to our terms and conditions to use this service");
-        // }
+        if (!check.checkValidity()) {
+            check.setCustomValidity("You must agree to our terms and conditions to use this service");
+        }
 
     });
+
+    // form.addEventListener("submit", function(e) {
+    //     console.log("Submit attempt");
+    //     if (!msg.checkValidity()) {
+    //         msg.setCustomValidity("Your message should be at least 10 characters, but no more than 500 characters");
+    //     }
+    //     else {
+    //         msg.setCustomValidity("");
+    //     }
+
+    //     msg.reportValidity();
+
+    //     let date = expDate.value;
+    //     let split = date.split("/");
+    //     let exp = new Date(split[1], split[0]);
+    //     let today = new Date();
+    //     if (!expDate.checkValidity()) {
+    //         expDate.setCustomValidity("Please match the following format: MM/YYYY");
+    //     }
+    //     else if (exp < today) {
+    //         expDate.setCustomValidity("The card you entered is expired");
+    //     }
+    //     else {
+    //         expDate.setCustomValidity("");
+    //     }
+        
+
+    //     if (!textPhone.checkValidity() && radioSMS.checked) {
+    //         textPhone.setCustomValidity("Please enter the 10 digit phone number you would like to notify ##########");
+    //     }
+
+    //     if (!textEmail.checkValidity() && radioEmail.checked) {
+    //         textPhone.setCustomValidity("Please enter the email you would like to notify");
+    //     }
+
+    //     if (!check.checkValidity()) {
+    //         console.log(check.checkValidity());
+    //         check.setCustomValidity("You must agree to our terms and conditions to use this service");
+    //     }
+    // })
 });
