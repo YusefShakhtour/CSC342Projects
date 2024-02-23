ead6202a6efd911e5a27aec1c6f5f0c5b68ebeac
yfshakht <yfshakht@ncsu.edu>
2024-02-23 11:24:57 -0500

    [Partial Implementation]

--- Homework3/server.js
+++ Homework3/server.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Import our Express dependency
 const multer = require('multer');
-const upload = multer({dest: 'file_uploads/'});
+const upload = multer({dest: '/static/uploads/'});
 
 const app = express(); // Create a new server instance
 const PORT = 3000; // Port number we want to use of this server
@@ -16,13 +16,39 @@ app.get('/', (req, res) => {
   res.sendFile(html_path + 'form.html');
 });
 
-app.get('/success', (req, res) => {
-  res.sendFile(html_path + 'success.html');
-});
+// app.get('/success', (req, res) => {
+//   res.sendFile(html_path + 'success.html');
+// });
 
-app.get('/error', (req, res) => {
+// app.get('/error', (req, res) => {
+//     res.sendFile(html_path + 'error.html');
+//   });
+
+app.post('/send', upload.single('myFile'), (req,res) => {
+  try {
+    if (req.body.expDate != "" && req.body.sendFirst != "" && req.body.sendLast != "" && req.body.recFirst != "" && 
+        req.body.recLast != "" && req.body.message != "" &&  req.body.cardType != "" && req.body.cardNum != "" && 
+        req.body.expDate != "" && req.body.ccv != "" && req.body.amount != "") {
+          if (req.body.emailRad == "on") {
+            if (req.body.email.checkValidity()) {
+              res.sendFile(html_path + 'error.html');
+            } 
+          }
+          else if (req.body.smsRad == "on") {
+            if (req.body.phone.checkValidity() == false) {
+              res.sendFile(html_path + 'error.html');
+            } 
+          }
+      res.sendFile(html_path + 'success.html');
+    }
+    else {
+      res.sendFile(html_path + 'error.html');
+    }
+  }
+  catch(err) {
     res.sendFile(html_path + 'error.html');
-  });
+  }
+})
 
 
 // As our server to listen for incoming connections
--- Homework3/static/js/form.js
+++ Homework3/static/js/form.js
@@ -1,4 +1,116 @@
 document.addEventListener('DOMContentLoaded', e => {
 
+    let submit = document.getElementById('submit');
+    let radioEmail = document.getElementById("email");
+    let radioSMS = document.getElementById("sms");
+    let radioNeither = document.getElementById("none");
+    let expDate = document.getElementById("date");
+    let textPhone = document.getElementById("textPhone");
+    let textEmail = document.getElementById("textEmail");
+    let msg = document.getElementById('msg');
+    let cardNum = document.getElementById('card');
 
+    let check = document.getElementById('check');
+    let file = document.getElementById("fileIn");
+    
+
+    radioSMS.addEventListener('click', function(e) {
+        textPhone.required = true;
+        textEmail.required = false;
+        radioEmail.checked = false;
+        radioNeither.checked = false;
+    })
+
+    radioEmail.addEventListener('click', function(e) {
+        textPhone.required = false;
+        textEmail.required = true;
+        radioSMS.checked = false;
+        radioNeither.checked = false;
+
+    })
+
+    radioNeither.addEventListener('click', function(e) {
+        textPhone.required = false;
+        textEmail.required = false;
+        radioSMS.checked = false;
+        radioEmail.checked = false;
+    })
+
+    file.addEventListener('change', function(e) {
+        let input = e.target;
+        let img = document.getElementById("preview");
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function(e) {
+                img.src = e.target.result;
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+
+    cardNum.setCustomValidity("Please enter your card in the following format ####-####-####-####");
+    check.setCustomValidity("You must agree to our terms and conditions to use this service");
+    msg.setCustomValidity("Your message should be at least 10 characters, but no more than 500 characters");
+    let date = expDate.value;
+    let split = date.split("/");
+    let exp = new Date(split[1], split[0]);
+    let today = new Date();
+    if (!expDate.checkValidity()) {
+        expDate.setCustomValidity("Please match the following format: MM/YYYY");
+    }
+    else if (exp < today) {
+        expDate.setCustomValidity("The card you entered is expired")
+    }
+
+    if (!textPhone.checkValidity() && radioSMS.checked) {
+        textPhone.setCustomValidity("Please enter the 10 digit phone number you would like to notify ##########");
+    }
+
+    if (!textEmail.checkValidity() && radioEmail.checked) {
+        textPhone.setCustomValidity("Please enter the email you would like to notify");
+    }
+
+    if (!check.checkValidity()) {
+        console.log(check.checkValidity());
+        check.setCustomValidity("You must agree to our terms and conditions to use this service");
+    }
+
+    submit.addEventListener('click', function(e) {
+        // console.log(cardNum.checkValidity());
+        // console.log(msg.checkValidity());
+        // console.log(expDate.checkValidity());
+        // console.log(check.checkValidity());
+        // console.log(file.checkValidity());
+
+
+        // if (msg.checkValidity() == false) {
+        //     msg.setCustomValidity("Your message should be at least 10 characters, but no more than 500 characters");
+        // }
+
+        // let date = expDate.value;
+        // let split = date.split("/");
+        // let exp = new Date(split[1], split[0]);
+        // let today = new Date();
+        // if (!expDate.checkValidity()) {
+        //     expDate.setCustomValidity("Please match the following format: MM/YYYY");
+        // }
+        // else if (exp < today) {
+        //     expDate.setCustomValidity("The card you entered is expired")
+        // }
+        
+
+        // if (!textPhone.checkValidity() && radioSMS.checked) {
+        //     textPhone.setCustomValidity("Please enter the 10 digit phone number you would like to notify ##########");
+        // }
+
+        // if (!textEmail.checkValidity() && radioEmail.checked) {
+        //     textPhone.setCustomValidity("Please enter the email you would like to notify");
+        // }
+
+        // if (!check.checkValidity()) {
+        //     console.log(check.checkValidity());
+        //     check.setCustomValidity("You must agree to our terms and conditions to use this service");
+        // }
+
+    });
 });
